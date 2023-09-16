788ccf9f84af382602621a62ed8e13e57fbeea88
bowenaidan <rivott35@hotmail.com>
2023-09-16 15:31:06 -0500

    finish cracking function

--- brute_force/password_cracker.py
+++ brute_force/password_cracker.py
@@ -1,4 +1,12 @@
-from vigenere_cipher import vigenere_encrypt, vigenere_decrypt
+import time
+from vigenere_cipher import vigenere_decrypt
+
+dict = set()
+with open("dict.txt", "r") as dictionary:
+    for line in dictionary:
+        entry = line.strip()
+        dict.add(entry)
+        
 
 def keyGen(keyLength, prefix=''):
     if keyLength == 0:
@@ -9,14 +17,32 @@ def keyGen(keyLength, prefix=''):
                 yield combo
 
 
-def password_crack(ciphertext, keyLength, firstWorldLength):
-    combinations = keyGen(keyLength, "")
-    for combination in combinations:
-        print(combination)
+def password_crack(ciphertext, keyLength, firstWordLength):
+    possible_translations = []
+    for key in keyGen(keyLength):
+        decrypted = vigenere_decrypt(ciphertext[:firstWordLength], key)
+        if decrypted in dict:
+            possible_translations.append(vigenere_decrypt(ciphertext, key))
+            return possible_translations
+
         
-
-ciphertext = "FEJKLVENAIHOVLEFJE"
-firstWorldLength = 5
-
-password_crack(ciphertext, 4, firstWorldLength)
-# password_crack("MSOKKJCOSXOEEKDTOSLGFWCMCHSUSGX", 2, 6)
+print("1. ")
+start_time = time.time()
+print(password_crack("MSOKKJCOSXOEEKDTOSLGFWCMCHSUSGX", 2, 6))
+print("--- %s seconds ---" % round((time.time() - start_time), 5))
+print("2. ")
+start_time = time.time()
+print(password_crack("PSPDYLOAFSGFREQKKPOERNIYVSDZSUOVGXSRRIPWERDIPCFSDIQZIASEJVCGXAYBGYXFPSREKFMEXEBIYDGFKREOWGXEQSXSKXGYRRRVMEKFFIPIWJSKFDJMBGCC", 3, 7))
+print("--- %s seconds ---" % round((time.time() - start_time), 5))
+print("3. ")
+start_time = time.time()
+print(password_crack("MTZHZEOQKASVBDOWMWMKMNYIIHVWPEXJA", 4, 10))
+print("--- %s seconds ---" % round((time.time() - start_time), 5))
+print("4. ")
+start_time = time.time()
+print(password_crack("SQLIMXEEKSXMDOSBITOTYVECRDXSCRURZYPOHRG", 5, 11))
+print("--- %s seconds ---" % round((time.time() - start_time), 5))
+print("5. ")
+start_time = time.time()
+print(password_crack("LDWMEKPOPSWNOAVBIDHIPCEWAETYRVOAUPSINOVDIEDHCDSELHCCPVHRPOHZUSERSFS", 6, 9))
+print("--- %s seconds ---" % round((time.time() - start_time), 5))
