9531c8e9383a4122030f7a80d8f2ef6ca4efa625
bowenaidan <rivott35@hotmail.com>
2023-09-16 15:31:48 -0500

    adjust decrypt to optitimize

--- brute_force/vigenere_cipher.py
+++ brute_force/vigenere_cipher.py
@@ -3,21 +3,18 @@ def vigenere_encrypt(plain_text, key):
     for i, char in enumerate(plain_text):
         index = i % len(key)
         if(char.isupper()):
-            encrypted_value = chr(((ord(char) + ord(key[index]) - 65 - 65) % 26) + 65)
+            encrypted_value = chr(((ord(char) + ord(key[index]) - 130) % 26) + 65)
             encrypted_text = encrypted_text + encrypted_value
         else:
-            encrypted_value = chr(((ord(char) + ord(key[index]) - 97 - 97) % 26) + 97)
+            encrypted_value = chr(((ord(char) + ord(key[index]) - 194) % 26) + 97)
             encrypted_text += encrypted_value
     return encrypted_text
 
+# optimized version (doesn't account for lower case)
 def vigenere_decrypt(encrypted_text, key):
-    decrypted_text = ""
+    decrypted_text = []
+    key_length = len(key)
     for i, char in enumerate(encrypted_text):
-        index = i % len(key)
-        if(char.isupper()):
-            decrypted_value = chr(((ord(char) - ord(key[index]) + 26) % 26) + 65)
-            decrypted_text = decrypted_text + decrypted_value
-        else:
-            decrypted_value = chr(((ord(char) - ord(key[index]) + 26) % 26) + 97)
-            decrypted_text += decrypted_value
-    return decrypted_text
+        shift = (ord(char) - ord(key[i % key_length]) + 26) % 26 + 65
+        decrypted_text.append(chr(shift))
+    return ''.join(decrypted_text)
